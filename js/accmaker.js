06f632879f1d31b56787c9554b711edcd41a99e0
tanjoin <tanjoin@users.noreply.github.com>
2017-04-06 16:46:31 +0900

    Update accmaker.js

--- js/accmaker.js
+++ js/accmaker.js
@@ -1,8 +1,12 @@
 (function(){
-  window.open(
-    "https://tanjoin.github.io/acc/maker.html" +
-    "?title=" + encodeURIComponent(document.title) +
-    "&url=" + encodeURIComponent(document.URL) +
-    "&description=" + encodeURIComponent(document.getSelection())
-  );
+  var url = "https://tanjoin.github.io/acc/maker.html" +
+      "?title=" + encodeURIComponent(document.title) +
+      "&url=" + encodeURIComponent(document.URL);
+  var d = document.getSelection();
+  var links = d.getRangeAt(0).cloneContents().querySelectorAll("a");
+  for (var i = 0; i < links.length; i++) {
+    url += "url[" + i + "]=" + links[i];
+  }
+  url += "&description=" + encodeURIComponent(d);
+  window.open(url);
 })();
