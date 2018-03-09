d45c6144956bd295908c7461b39958f115f075d2
tanjoin <tanjoin@users.noreply.github.com>
2018-03-10 02:39:42 +0900

    MIME Type の問題と const -> var

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ jQuery があることを前提に作成
 ## 長いやつ
 
 ```
-(function(){var s = document.createElement("script");s.src = "https://raw.githubusercontent.com/tanjoin/bookmarklet/gh-pages/js/サンプル.js";document.body.appendChild(s);})();
+(function(){var s = document.createElement("script");s.src = "https://tanjoin.github.io/bookmarklet/js/サンプル.js";document.body.appendChild(s);})();
 ```
--- js/joysound_myhistory.js
+++ js/joysound_myhistory.js
@@ -1,5 +1,5 @@
-const list = document.getElementById('usk-mypage-history-index-list').children;
-const result = '';
+var list = document.getElementById('usk-mypage-history-index-list').children;
+var result = '';
 for (var i = 0; i < list.length; i++) {
   var data = list[i].children[0].children[0].children[0].innerText.split('／');
   var date = list[i].children[0].children[0].children[2].innerText.substr(5);
