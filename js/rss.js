ecd141cdfa37b90add1a465196f9e592fe35cce4
tanjoin <tanjoin@users.noreply.github.com>
2018-06-26 15:30:23 +0900

    Atom も

--- js/rss.js
+++ js/rss.js
@@ -1,1 +1,1 @@
-[...document.querySelectorAll('head > link')].filter((link) => link.type === "application/rss+xml").forEach((link) => alert(link.href));
+[...document.querySelectorAll('head > link')].filter((link) => link.type === "application/rss+xml" || link.type === "application/atom+xml").forEach((link) => alert(link.href));
