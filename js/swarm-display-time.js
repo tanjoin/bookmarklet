aa8e400f86d0eb4af37df9e8ffaf2815761a7078
tanjoin <1418838+tanjoin@users.noreply.github.com>
2022-07-27 18:05:05 +0900

    Update swarm-display-time.js

--- js/swarm-display-time.js
+++ js/swarm-display-time.js
@@ -1,7 +1,7 @@
 [...document.querySelectorAll("span.timestamp")]
   .forEach((span) => {
     const date = new Date(span.getAttribute('data-created-at') * 1000);
-    const text = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2) + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
+    const text = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2) + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
     var s = document.createElement('div');
     s.innerText = text;
     span.parentNode.appendChild(s);
